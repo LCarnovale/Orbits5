4afd82ed006a6432ce57bca0d11a52d4f91816c0
Leo Carnovale <Leo.carnovale@gmail.com>
2018-09-17 13:33:32 +1000

    Initial commit

---
+++ Orbits5.py
@@ -0,0 +1,13 @@
+import math, turtle, sys, random
+import vector as VECTOR
+import particle as PARTICLE
+
+vector = VECTOR.vector()
+randomVector = VECTOR.randomVector
+
+class Item:
+	def __init__(self, position = )
+
+class MainLoop:
+	def __init__(self, items = []):
+		self.items = items
